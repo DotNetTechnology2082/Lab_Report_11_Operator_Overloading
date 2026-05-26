89d10fce865724815801764651dd26910015f021
Namrata <namratabca23@gmail.com>
2026-05-26 07:57:11 +0545

    this is lab 11

--- Lab_Report_11_Operator_Overloading/Program.cs
+++ Lab_Report_11_Operator_Overloading/Program.cs
@@ -1,1 +1,49 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Complex
+{
+    int real, imaginary;
+
+    // Constructor
+    public Complex(int r, int i)
+    {
+        real = r;
+        imaginary = i;
+    }
+
+    // Operator Overloading
+    public static Complex operator +(Complex c1, Complex c2)
+    {
+        return new Complex(
+            c1.real + c2.real,
+            c1.imaginary + c2.imaginary
+        );
+    }
+
+    // Method Overloading
+    public void Display()
+    {
+        Console.WriteLine("Complex Number: " + real + " + " + imaginary + "i");
+    }
+
+    public void Display(string msg)
+    {
+        Console.WriteLine(msg + ": " + real + " + " + imaginary + "i");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Complex c1 = new Complex(2, 3);
+        Complex c2 = new Complex(4, 5);
+
+        Complex c3 = c1 + c2;
+
+        c3.Display();
+        c3.Display("Result by Namrata");
+
+        Console.ReadLine();
+    }
+}
